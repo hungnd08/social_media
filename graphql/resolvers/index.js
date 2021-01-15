0083586a4c2c9bf9709eb7673635d14b5f5209df
ddhung <ddhung@ambition.vn>
2021-01-15 16:50:17 +0700

    Setting Up Database & Server, Register

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+const usersResolvers = require('./users');
+const postsResolvers = require('./posts');
+
+module.exports = {
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation
+    }
+}
