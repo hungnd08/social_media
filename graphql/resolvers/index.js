feb6a0748f31b9b52f09f8a416c51b29ea5e7fb5
ddhung <ddhung@ambition.vn>
2021-01-18 17:53:46 +0700

    Finishing up the server

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,21 @@
 const usersResolvers = require('./users');
 const postsResolvers = require('./posts');
+const commentsResolvers = require('./comments');
 
 module.exports = {
+    Post: {
+        likeCount: (parent) => parent.likes.length,
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
     Mutation: {
         ...usersResolvers.Mutation,
-        ...postsResolvers.Mutation
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    },
+    Subscription: {
+        ...postsResolvers.Subscription
     }
 }
