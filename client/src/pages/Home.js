617847b9c9d5b99bc0c49d3283f63db18a4bc9c1
ddhung <ddhung@ambition.vn>
2021-01-25 18:09:25 +0700

    Create post ver 1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Grid } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 import { FETCH_POSTS_QUERY } from '../util/graphql';
+import { AuthContext } from '../context/auth';
 
 function Home() {
+    const { user } = useContext(AuthContext);
     const {
         loading,
         data
@@ -16,6 +19,11 @@ function Home() {
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {user && (
+                    <Grid.Column>
+                        <PostForm/>
+                    </Grid.Column>
+                )}
                 {loading ? (
                     <h1>Loading posts..</h1>
                 ) : (
