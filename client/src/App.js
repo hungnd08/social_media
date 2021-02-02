bcf9d06d8541936d291c5b37021c4d33996aa573
ddhung <ddhung@ambition.vn>
2021-02-02 18:03:43 +0700

    Like function, single post page v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-import MenuBar from './components/MenuBar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-import { AuthProvider } from './context/auth';
-import AuthRoute from './util/AuthRoute';
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
+import SinglePost from "./pages/SinglePost";
 
 function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <Container>
-                    <MenuBar />
-                    <Route exact path='/' component={Home} />
-                    <AuthRoute exact path='/login' component={Login} />
-                    <AuthRoute exact path='/register' component={Register} />
-                </Container>
-            </Router>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
+        </Container>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
